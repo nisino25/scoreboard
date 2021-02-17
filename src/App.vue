0c12d0228bb3f9bfbfd2ea6592c6e29258d60a21
nisino25 <nisino25@gmail.com>
2021-02-16 23:37:06 -0800

    added more option for dates

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-  <input type="text" v-model="searchDate" >
-  <button @click="getScores()">Get the score of {{searchDate}} on console</button>
+  <input type="text" v-model="searchDate" ><br>
+  <button>Go back</button>
+  <button @click="getScores()">Get the score of </button> 
+  <button>Next</button>
+  <br><br>
+  <button @click="getScores('yesterday')">Yesterday</button>
+  <button @click="getScores('today')">Today</button>
+  <button @click="getScores('tomorrow')">Tomorrow</button>
+  <br><br>
 
 
-  <div v-if="gameData">
+  <div v-if="gameData && !(isFetchingData) && !(hasFailed)">
     <div v-for="(game, i) in gameData.games" :key="i">
       <strong>{{game.hTeam.triCode}}</strong>&nbsp; <small>{{game.hTeam.score}}</small>&nbsp;
       <!-- <strong>{{game.period.current}}</strong>&nbsp; -->
@@ -13,9 +19,19 @@
       <span style="color:red" v-else-if="game.period.current !==0">{{game.period.current}}Q {{game.clock}}</span>
       <small  v-else-if="game.period.current === 0" >{{game.startTimeEastern}}</small>
       <hr>
+      
 
     </div>
   </div>
+  <div v-if="gameData &&gameData.games.length === 0 && !(isFetchingData)"><br><br><br> No Games</div>
+  <div v-if="isFetchingData">
+    <br><br><br>
+    <strong>Loading.....</strong>
+  </div>
+  <div v-if="hasFailed && !(isFetchingData)"><br><br><br>
+    Sorry the latest data you can get is 
+  </div>
+  
   
 </template>
 
@@ -28,6 +44,11 @@ export default {
       searchDate: undefined,
       isFetchingData: false,
       gameData: undefined,
+      displayDate: undefined,
+
+
+
+      hasFailed: false,
 
 
 
@@ -36,39 +57,79 @@ export default {
     }
   },
   methods:{
-    async getScores() {
-      // this.showingMovie = true
-      // this.decades = false;
-      // this.pageForMyLists = false;
-      // this.multipleYears = true;
-      // this.multipleContents = false;
-      // this.count =0
-      // this.isImageLoaded = false
-      // this.condition = 'searching'
+    async getScores(selectedDate) {
+
+      switch(selectedDate){
+        case 'yesterday':
+          // var d = new Date();
+          // d.setDate(d.getDate() - 1);
+
+          var today = new Date()
+          var yesterday = new Date(today)
+          yesterday.setDate(yesterday.getDate() - 1)
+          var y = yesterday.getFullYear();
+          var m = yesterday.getMonth() + 1;
+          var d = yesterday.getDate();
+          var mm = m < 10 ? '0' + m : m;
+          var dd = d < 10 ? '0' + d : d;
+          this.searchDate  = '' + y + mm + dd
+
+          // var now = new Date()
+          // var y = now.getFullYear();
+          // var m = now.getMonth() + 1;
+          // var d = now.getDate();
+          // var mm = m < 10 ? '0' + m : m;
+          // var dd = d < 10 ? '0' + d : d;
+          // this.searchDate  = '' + y + mm + dd
+
+          break;
+
+        case 'today':
+          var now = new Date();
+          y = now.getFullYear();
+          m = now.getMonth() + 1;
+          d = now.getDate();
+          mm = m < 10 ? '0' + m : m;
+          dd = d < 10 ? '0' + d : d;
+          this.searchDate  = '' + y + mm + dd
+          break;
+
+        case 'tomorrow':
+          today = new Date()
+          var tomorrow = new Date(today)
+          tomorrow.setDate(tomorrow.getDate() + 1)
+          y = tomorrow.getFullYear();
+          m = tomorrow.getMonth() + 1;
+          d = tomorrow.getDate();
+          mm = m < 10 ? '0' + m : m;
+          dd = d < 10 ? '0' + d : d;
+          this.searchDate  = '' + y + mm + dd
+          break;
+        
+        
+      }
       this.isFetchingData = true
-      // const query = this.query
 
 
       const URL = 'https://data.nba.net/10s/prod/v1/'+ this.searchDate + '/scoreboard.json'
       // const URL = 'http://data.nba.net/10s/prod/v1/20210215/scoreboard.json'
       console.log('Calling API')
       console.log(URL)
+
+
       const res = await fetch(URL)
       const json = await res.json()
       console.log(json)
-      this.gameData = json 
-      console.log(this.gameData.games[0].hTeam.triCode)
-      console.log(this.gameData.games[0].hTeam.score)
-      
-      this.isFetchingData = false
-      // if(json.success === false){
-      //   this.condition = 'error'
-      // }else{
-      //   this.condition = 'randomSuccess'
-      // }
-      // if (query !== this.query) {
-      //   await this.searchMovie()
-      // }
+      if(json.Message !== 'Object not found.'){
+        this.hasFailed = false
+        this.gameData = json 
+        this.isFetchingData = false
+      }else{
+        this.hasFailed = true
+        this.isFetchingData = false;
+      }
+     
+      // console.log(this.gameData.games.length)
       
     },
   },
